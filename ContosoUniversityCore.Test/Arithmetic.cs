7f120512cdf707e64d553dc528744e097061ad73
anzuedwi <anzuedwi@gmail.com>
2018-02-26 05:51:14 -0600

    Adding Testing XUnit
    XUnit Testing Framework

---
+++ ContosoUniversityCore.Test/Arithmetic.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+
+namespace ContosoUniversityCore.Test
+{
+    public class Arithmetic
+    {
+        [Fact]
+        public void MathTest()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+    }
+}
